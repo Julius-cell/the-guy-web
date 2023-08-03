5599c277c41bb03c885e133a04e03d33c1b6b861
julius-cell <julio.cid.b@gmail.com>
2023-08-02 23:27:17 -0300

    feat: :sparkles: add navbar component

--- packages/library-sb/src/stories/or-header/Header.tsx
+++ packages/library-sb/src/stories/or-header/Header.tsx
@@ -2,33 +2,10 @@ import { useEffect, useState } from 'react';
 
 import { Button } from '../at-button/Button';
 import { Image } from '../at-image/Image';
-import { Link } from '../at-link/Link';
+import { NavBar } from '../ml-navbar/Navbar';
 
-import type { HeaderProps, LinkProps } from '../../types/components-type-props';
-import type { ContentfulAsset, MediaWrapperProps } from '../../types/contentful-types';
-
-const NavBar = (props: { categories?: LinkProps[]; languajes?: MediaWrapperProps[] }) => {
-  return (
-    <nav className="h-[5%] flex w-full absolute justify-between">
-      <div className="h-full flex bg-white rounded-lg border-2">
-        {props.languajes?.map((languaje, index) => {
-          // const handleClick = (event: any) => {
-          //   //Add changing languajes function
-          // };
-          return (
-            <div key={index}>
-              {/* <div onClick={(event) => handleClick(event)}> */}
-              <Image desktopAsset={languaje.desktopAsset} className="h-full ease-in duration-200 hover:scale-125" />
-            </div>
-          );
-        })}
-      </div>
-      <ul className="flex justify-end space-x-10">
-        {props.categories?.map((categorie, index) => <Link key={index} {...categorie} />)}
-      </ul>
-    </nav>
-  );
-};
+import type { HeaderProps } from '../../types/components-type-props';
+import type { ContentfulAsset } from '../../types/contentful-types';
 
 export const Header = ({ hero, categories, languajes = [] }: HeaderProps) => {
   const [heroImage, setHeroImage] = useState<ContentfulAsset | undefined>(undefined);
