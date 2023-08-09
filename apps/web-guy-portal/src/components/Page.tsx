297c3a9ca86c18110c21a3df30cba1dd2ac47c59
julius-cell <julio.cid.b@gmail.com>
2023-08-09 17:57:00 -0300

    fix: :fire: update grid section for redundance

--- apps/web-guy-portal/src/components/Page.tsx
+++ apps/web-guy-portal/src/components/Page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardProps, GridSection, GridSectionProps, Header, ModulesComponents } from 'library-sb';
+import { Card, CardProps, RichText, Header, ModulesComponents, RichTextProps } from 'library-sb';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { getPage } from '../contentful/get-page';
 import { ContentfulPage } from '../types/page-types';
@@ -6,7 +6,7 @@ import { ContentfulPage } from '../types/page-types';
 const renderModuleComponent = (modulesList: string[], args: CardProps[]): ReactNode[] => {
   const modulesComponents: ModulesComponents = {
     mlCard: Card,
-    orGridSection: GridSection,
+    richText: RichText,
   };
 
   return modulesList.map((module: string, index: number) => (
@@ -17,14 +17,14 @@ const renderModuleComponent = (modulesList: string[], args: CardProps[]): ReactN
 const Page = () => {
   const [page, setPage] = useState<ContentfulPage | undefined>(undefined);
   const [modules, setModules] = useState<string[]>([]);
-  const [moduleArgs, setModuleArgs] = useState<CardProps[]>([]);
+  const [moduleArgs, setModuleArgs] = useState<(CardProps | RichTextProps)[]>([]);
 
-  const mapContentTypesModules = (modulesArray: (CardProps | GridSectionProps)[]) => {
+  const mapContentTypesModules = (modulesArray: (CardProps | RichTextProps)[]) => {
     for (let index = 0; index < modulesArray.length; index++) {
       const { contentTypeId, ...args } = modulesArray[index];
 
       setModules((prevModules: string[]) => [...prevModules, contentTypeId || '']);
-      setModuleArgs((prevArgs: (CardProps | GridSectionProps)[]) => [...prevArgs, args]);
+      setModuleArgs((prevArgs: (CardProps | RichTextProps)[]) => [...prevArgs, args]);
     }
   };
 
@@ -43,7 +43,7 @@ const Page = () => {
   return (
     <div className="grid grid-cols-4 mx-10">
       <Header categories={page?.header?.categories} hero={page?.header?.hero} languajes={page?.header?.languajes} />
-      <main className="flex flex-col min-h-screen space-y-1/2 mt-1/10">
+      <main className="col-span-full min-h-screen space-y-1/2 mx-20">
         {modules.length ? renderModuleComponent(modules, moduleArgs) : <></>}
       </main>
     </div>
