035dba2c68252ce376175c5ea2be190afdc6f2d4
julius-cell <julio.cid.b@gmail.com>
2023-08-11 18:16:27 -0300

    feat: :sparkles: add experience section component & collapsible component

--- packages/library-sb/src/main.tsx
+++ packages/library-sb/src/main.tsx
@@ -3,8 +3,8 @@ export * from './stories/at-image/Image';
 export * from './stories/at-richtext/RichText';
 
 export * from './stories/ml-card/Card';
-export * from './stories/ml-experience-section/Experience-section';
 
+export * from './stories/or-experience-section/Experience-section';
 export * from './stories/or-header/Header';
 
 export * from './types/components-type-props';
